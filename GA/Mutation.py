34d47a8171b5fabeb31135cd2b488cb52fe412d6
Dieka Nugraha K <dieka13@yahoo.co.id>
2016-09-19 19:39:08 +0700

    Simple GA Project initialization

---
+++ GA/Mutation.py
@@ -0,0 +1,7 @@
+"""
+Simple Genetic Algorithm Implementation in Python
+Artificial Intelligence Laboratory - Telkom University - Indonesia
+Dieka Nugraha K - 1103120057
+
+Collection of commonly used Mutation method
+"""
